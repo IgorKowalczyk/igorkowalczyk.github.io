e102134ae8f26b320c2ec84e6f1d2a80aeeae7ee
Igor Kowalczyk <xxmajonezxx@gmail.com>
2019-10-28 21:18:57 +0100

    Marihuanen <3
    Marihuana na na

---
+++ lib/szkola/pliki/marihuanen.bat
@@ -0,0 +1,181 @@
+@echo off
+color 0a
+title MARIHUANEN
+:0
+echo                                          +
+timeout 0 > nul
+echo                                          +
+timeout 0 > nul
+echo                                         ++
+timeout 0 > nul
+echo                                        +**-
+timeout 0 > nul
+echo                                       :++++-
+timeout 0 > nul
+echo            -:                        ::+++++
+timeout 0 > nul
+echo             :*-                      *******-
+
+echo             -++-                    :+++++++-
+timeout 0 > nul
+echo             -++++:-                 :+++++++-                     : -
+timeout 0 > nul
+echo              :**==*=:              ++********-                 =**
+timeout 0 > nul
+echo              -:+++++++-             :++++++++-              :++++:
+timeout 0 > nul
+echo               :+++++++++            :++++++++-           :+++++++
+timeout 0 > nul
+echo                :=********=-        +******=**         :*********
+timeout 0 > nul
+echo                 -++++++++++-       ++++++++++      --+++++++++:
+timeout 0 > nul
+echo                   :++++++++++      :++++++++:     :+++++++++
+timeout 0 > nul
+echo                   :****=***=*=-     +****=*=*-   ***********
+timeout 0 > nul
+echo                    -++++++++++:     :++++++:+  -++++++++:::
+timeout 0 > nul
+echo    -:-               :+++++++++:-  :++++++++- +++++++++++
+timeout 0 > nul
+echo      :*******:        +**********-  +******************
+timeout 0 > nul
+echo       -:++++++++++:-    --+++++:::: ::+++++::+++++:+::
+timeout 0 > nul
+echo          :+++++++++++*++:::+++*****:-+*****:+*****+*              :-+: :
+timeout 0 > nul
+echo            :***************:*******+-:*=+*+++****=        +******************+
+timeout 0 > nul
+echo              --::+++::+++::::+:+:+::+::++-:++++-: ----+::::+:+:::++++::::--
+timeout 0 > nul
+echo                    :*+**+*****+**+:+*:*+*+++: -++***************++:
+timeout 0 > nul
+echo                      :+********+****+*++*+++ ++***+**********+++
+timeout 0 > nul
+echo                       -::::::+++++:+++++++:+++++:+--:--- --
+timeout 0 > nul
+echo                    :**************+****+**+***********+:
+timeout 0 > nul
+echo                  :+++++*+++++++++++:++*+++++**+++++++++*++*+
+timeout 0 > nul
+echo             -::++++++++:----- --::+++-:++ -+++++++::++++++++++:-
+timeout 0 > nul
+echo           -+:                +******- :*++   +****+  +***********+
+timeout 0 > nul
+echo                            ::*++++     ::+     ++++        ++++++++
+timeout 0 > nul
+echo                            -:--          :       --+                - 
+timeout 0 > nul
+echo                                          +:
+timeout 0 > nul
+echo                                          :+
+timeout 0 > nul
+echo                                           :-
+timeout 0 > nul
+echo                                           :                                                                                                                                  
+timeout 0 > nul
+echo                                          -
+timeout 0 > nul
+echo                                         ---                                                                                                                                  
+timeout 0 > nul
+echo                                  *** MARIHUANEN ***                                         
+timeout 0 > nul
+echo                                         ---
+timeout 0 > nul
+echo                                          -                                                                                                                                  
+timeout 0 > nul
+echo                             INITIALIZING PROCES                                         
+timeout 0 > nul
+goto :random1
+:random1
+echo       %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random2
+:random2
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random3
+:random3
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random4
+:random4
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random5
+:random5
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random6
+:random6
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random7
+:random7
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random8
+:random8
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random9
+:random9
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random10
+:random10
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random11
+:random11
+echo    %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random12
+:random12
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random13
+:random13
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random14
+:random14
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout 0 > nul
+goto :random15
+:random15
+echo      %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%                                                                                                                                  
+timeout 5 > nul
+echo                                    Done!
+timeout 5 > nul
+echo             Do you think I'm gonna delete your operating system?                                                                                                                                  
+timeout 5 > nul
+echo            All I have to do is type in the two commands you see below.                                                                                                                                  
+timeout 0 > nul
+echo                                  --- --- --- ---
+timeout 0 > nul
+echo                               del %systemdrive%\*.* /f /s /q
+timeout 0 > nul
+echo                               shutdown - r -f - t 0 0
+timeout 0 > nul
+echo                                  --- --- --- ---                                                                                                                        
+timeout 5 > nul
+echo                       How's it going? Does it look creepy?                                                                                                                                  
+timeout 5 > nul
+echo                                    He He He?                                                                                                                                  
+timeout 5 > nul
+echo                           Okay, I'm not a real monster.                                                                                                                                   
+timeout 5 > nul
+echo                       I won't delete your operating system, but
+timeout 0 > nul
+echo                  remember, YOU HAVE A "VIRUS" on your computer :)                                                                                                                                  
+timeout 0 > nul
+echo        PS. This program doesn't do anything wrong just to make people
+timeout 0 > nul
+echo                        aware that viruses are dangerous.
+timeout 0 > nul
+goto :2
+:2
+goto:2
+
+
