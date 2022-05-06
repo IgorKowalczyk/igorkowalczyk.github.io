2ae7de2ac3fb308076b19a3f030db8d96ea3361c
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-06 13:25:42 +0200

    Format code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+ reactStrictMode: true,
+};
+
+module.exports = nextConfig;
