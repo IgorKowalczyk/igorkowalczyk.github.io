3c3d1415b72e52f432eb7081dc9262b8bfd5de89
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-08 21:51:58 +0200

    Fix next session

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import AppProps from "next/app";
 import { ThemeProvider } from "next-themes";
-import { SessionProvider } from "next-auth/react";
 import "@styles/globals.css";
 import { UseAnalytics } from "@lib/analytics";
 
 export default function App({ Component, pageProps } = AppProps) {
  UseAnalytics();
  return (
-  <SessionProvider session={pageProps.session}>
    <ThemeProvider attribute="class">
     <Component {...pageProps} />
    </ThemeProvider>
-  </SessionProvider>
  );
 }
