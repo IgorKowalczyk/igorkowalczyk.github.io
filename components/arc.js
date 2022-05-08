a92294184954cee58494e78d7007d8aba02bfd39
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-08 10:15:10 +0200

    Format code

--- components/arc.js
+++ components/arc.js
@@ -3,9 +3,7 @@ import { Component } from "react";
 export default class Arc extends Component {
  render() {
   if (process.env.NODE_ENV === "production") {
-   return (
-   <script async src={'https://arc.io/widget.min.js#' + process.env.NEXT_PUBLIC_ARC_TOKEN}></script>
-   );
+   return <script async src={"https://arc.io/widget.min.js#" + process.env.NEXT_PUBLIC_ARC_TOKEN}></script>;
   } else {
    return;
   }
