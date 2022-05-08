aaa01448f0c80080608e7cd92a6c82ea01d26c67
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-08 20:36:54 +0200

    Fix build

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { AppProps } from "next/app";
+import AppProps from "next/app";
 import { ThemeProvider } from "next-themes";
 import { SessionProvider } from "next-auth/react";
 import "@styles/globals.css";
