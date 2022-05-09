8780c03debaf3ae1fdb1ab058edca09250b365f1
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-09 09:09:21 +0200

    Add loader to page

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import AppProps from "next/app";
 import { ThemeProvider } from "next-themes";
-import "@styles/globals.css";
 import { UseAnalytics } from "@lib/analytics";
+import { Router } from "next/router";
+import nProgress from "nprogress";
+import "@styles/progress.css";
+import "@styles/globals.css";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 export default function App({ Component, pageProps } = AppProps) {
  UseAnalytics();
  return (
-   <ThemeProvider attribute="class">
-    <Component {...pageProps} />
-   </ThemeProvider>
+  <ThemeProvider attribute="class">
+   <Component {...pageProps} />
+  </ThemeProvider>
  );
 }
