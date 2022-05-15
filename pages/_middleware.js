152266d147a72b140c2a916e4029cd5cb0741084
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-15 17:44:42 +0200

    Update referrer policy

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export function middleware() {
  const ContentSecurityPolicy = `
     default-src 'self' *.googletagmanager.com *.arc.io;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.googletagmanager.com arc.io *.arc.io *.sentry-cdn.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com arc.io *.arc.io;
     child-src 'self' *.youtube.com *.google.com *.twitter.com *.arc.io;
     style-src 'self' 'unsafe-inline' *.googleapis.com *.arc.io *.cloudflare.com;
     img-src * blob: data:;
@@ -13,11 +13,12 @@ export function middleware() {
   `;
  const response = NextResponse.next();
  response.headers.set("Content-Security-Policy", ContentSecurityPolicy.replace(/\n/g, ""));
- response.headers.set("Referrer-Policy", "origin-when-cross-origin");
+ response.headers.set("Referrer-Policy", "origin no-referrer ");
  response.headers.set("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
  response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
  response.headers.set("X-Frame-Options", "sameorigin");
  response.headers.set("X-Content-Type-Options", "nosniff");
  response.headers.set("X-DNS-Prefetch-Control", "on");
+ response.headers.set("X-XSS-Protection", "1; mode=block");
  return response;
 }
