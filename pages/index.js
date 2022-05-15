6bfcdcd57c2e04257565ff18aa98600d79442f6e
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-15 22:03:11 +0200

    Add gradient in main page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import { setContext } from "@apollo/client/link/context";
 export default function main({ public_repos_data, private_repos_data }) {
  return (
   <Container>
+        <div className="will-change-contents absolute left-0 -top-1/2 bottom-0 right-0 pointer-events-none z-[-1] blur-[160px] dark:bg-[conic-gradient(from_230.29deg_at_51.63%_52.16%,#0086F526_0deg,#040d21_67.5deg,#040d21_198.75deg,#040d21_251.25deg,#0086F517_301.88deg,#040d21_1turn)] bg-[conic-gradient(from_230.29deg_at_51.63%_52.16%,#336FEF40_0deg,#fff_67.5deg,#fff_198.75deg,#fff_251.25deg,#336FEF2B_301.88deg,#fff_1turn)]"></div>
    <div className="mx-auto -mt-24 flex min-h-screen flex-1 flex-col justify-center duration-300 md:w-[90%] xl:w-4/5">
     <div className="md:grid-cols-0 grid lg:grid-cols-5">
      <div className="md:col-span-3">
@@ -65,7 +66,7 @@ export default function main({ public_repos_data, private_repos_data }) {
      </div>
     </div>
    </div>
-   <div className="text-center mx-auto" id="about"><h1 className="dark:color-black mx-0 mt-0 mb-0 bg-gradient-to-r from-[#712af6] to-[#1a8aec] box-decoration-clone bg-clip-text text-center font-poppins text-[20px] font-semibold tracking-[-0.03em] duration-300 text-fill-transparent dark:from-[#a2facf] dark:to-[#64acff] md:text-[20px] lg:text-[40px] xl:text-[55px]">Soon!</h1></div>
+   <div className="text-center mx-auto" id={'about'}><h1 className="dark:color-black mx-0 mt-0 mb-0 bg-gradient-to-r from-[#712af6] to-[#1a8aec] box-decoration-clone bg-clip-text text-center font-poppins text-[20px] font-semibold tracking-[-0.03em] duration-300 text-fill-transparent dark:from-[#a2facf] dark:to-[#64acff] md:text-[20px] lg:text-[40px] xl:text-[55px]">Soon!</h1></div>
   </Container>
  );
 }
