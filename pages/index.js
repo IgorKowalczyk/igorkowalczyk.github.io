12331d3e96e75e3d34bf18f35dc44b08a9d90a60
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-15 19:45:40 +0200

    Update links in main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Container from "@components/Container";
-import Link from "next/link";
+import NextLink from "next/link";
 import { config } from "@/config";
 import { ConvertBytes } from "@lib/convertBytes";
 import { ApolloClient, createHttpLink, InMemoryCache, gql } from "@apollo/client";
@@ -15,15 +15,15 @@ export default function main({ public_repos_data, private_repos_data }) {
       <h3 className="text-center font-poppins text-[1.5rem]  font-semibold md:text-left">{config.header.subtitle}</h3>
       <p className="mt-2 text-center font-poppins text-slate-600 dark:text-slate-400  md:w-3/4 md:text-left">{config.header.description}</p>
       <div className="mt-4 flex justify-center md:block">
-       <Link href="/about">
-        <a className="arrow animatedLink group relative mt-5 inline-block items-center justify-center p-2 pl-0 pr-0 pb-1 font-semibold">
+       <NextLink href="/about/"> 
+        <a className="arrow animatedLink group relative mt-5 inline-block items-center justify-center p-2 pl-0 pr-0 pb-1 font-semibold" href="/about">
          More about me
          <svg xmlns="http://www.w3.org/2000/svg" className="arrowSymbol inline-block translate-x-[5px] duration-200 group-hover:translate-x-[10px]" width="16" height="16" viewBox="0 0 16 16" fill="none">
           <path fill="currentColor" d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path>
           <path stroke="currentColor" d="M1.75 8H11" strokeWidth={2} strokeLinecap="round"></path>
          </svg>
         </a>
-       </Link>
+       </NextLink>
       </div>
      </div>
      <div className="hidden items-center md:col-span-3 md:mt-8 md:flex lg:col-span-2 smallDesktop:col-span-2">
@@ -34,9 +34,9 @@ export default function main({ public_repos_data, private_repos_data }) {
          <span className="font-semibold text-slate-700 duration-200 dark:text-slate-300">$</span>{" "}
          <span>
           list github --user=
-          <Link href={`https://github.com/${config.social.github.username}`}>
+          <NextLink href={`https://github.com/${config.social.github.username}`}>
            <a target="_blank">"{config.social.github.username}"</a>
-          </Link>
+          </NextLink>
          </span>
         </span>
         <br />
