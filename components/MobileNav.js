19caebc090bbd5a490d7d7db629b44f26529462d
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-18 13:28:47 +0200

    Update MobileNav.js

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -38,7 +38,7 @@ export default function MobileMenu() {
         return (
          <p key={index} className="mt-6 w-0 translate-x-[-16px] whitespace-nowrap border-b border-gray-300 text-sm font-semibold text-gray-900 opacity-0 duration-300 motion-reduce:transition-none dark:border-gray-700/75 dark:text-gray-100" style={{ transitionDelay: `${150 * index - 50}ms` }}>
           <Link href={item.href} key={index}>
-           <a className="flex w-auto pb-4" onClick={toggleMenu}>
+           <a className="flex w-auto pb-4" onClick={toggleMenu} target={item.target}>
             {item.title}
            </a>
           </Link>
