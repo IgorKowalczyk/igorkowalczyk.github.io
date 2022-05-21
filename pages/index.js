f6ed0485d470deed35bfb75624a7ead0702a4f12
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-05-21 21:49:35 +0200

    Format code

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function main({ repositories, user }) {
        </NextLink>
       </div>
      </div>
-     <div className="hidden items-center motion-reduce:transition-none md:col-span-3 md:mt-7 md:-mb-7 lg:mt-0 lg:mb-0 md:flex lg:col-span-2">
+     <div className="hidden items-center motion-reduce:transition-none md:col-span-3 md:mt-7 md:-mb-7 md:flex lg:col-span-2 lg:mt-0 lg:mb-0">
       <div className="block w-full rounded-md border-[1px] border-[#3391fc]/[40%] p-4 font-poppins text-sm shadow-codeLight transition-colors motion-reduce:transition-none dark:border-white/[10%] dark:bg-[#08152b] dark:shadow-codeDark	">
        <div>
         <span className="font-semibold leading-6 text-[#ea4aaa]">→</span> <span className="font-semibold text-[#66e777]">~/{config.header.code.default.user}</span>{" "}
@@ -112,7 +112,7 @@ export default function main({ repositories, user }) {
      </p>
     </div>
    </div>
-   <div className="mx-auto text-center scroll-mt-20" id={"about"}>
+   <div className="mx-auto scroll-mt-20 text-center" id={"about"}>
     <h3 className="dark:color-black m-6 bg-gradient-to-r from-[#712af6] to-[#1a8aec] box-decoration-clone bg-clip-text text-center font-poppins text-[35px] font-semibold tracking-[-0.03em] duration-300 text-fill-transparent motion-reduce:transition-none dark:from-[#a2facf] dark:to-[#64acff] md:text-[35px] lg:text-[37px] xl:text-[40px]">Most Popular Repositories</h3>
     <div className="relative">
      <div className="xl-grid-cols-4 mb-8 grid grid-cols-1 gap-y-10 gap-x-6 px-8 pb-4 text-center font-poppins text-black dark:text-white md:grid-cols-2 md:gap-x-10 lg:grid-cols-3">
@@ -151,17 +151,17 @@ export default function main({ repositories, user }) {
           {repo.description.substring(0, 90)}
           {repo.description.length > 90 ? "..." : ""}
          </p>
-         <div className="mt-2 flex gap-1 overflow-hidden overflow-x-auto hide-scrollbar snap-x snap-mandatory">
+         <div className="hide-scrollbar mt-2 flex snap-x snap-mandatory gap-1 overflow-hidden overflow-x-auto">
           <NextLink key="repo_lang" href={`${repo.url}/search?l=${repo.primaryLanguage ? repo.primaryLanguage.name : "Markdown"}`}>
            <a target="_blank">
-            <span className="snap-always snap-center flex w-max content-center items-center rounded-lg border-2 border-transparent bg-black/[5%] py-[0.12em] px-[0.5em] text-[88%] text-black/[60%] duration-200 motion-reduce:transition-none dark:bg-white/10 dark:text-white/[70%]">
+            <span className="flex w-max snap-center snap-always content-center items-center rounded-lg border-2 border-transparent bg-black/[5%] py-[0.12em] px-[0.5em] text-[88%] text-black/[60%] duration-200 motion-reduce:transition-none dark:bg-white/10 dark:text-white/[70%]">
              <span className="mr-1 block h-[12px] w-[12px] rounded-full" style={{ backgroundColor: repo.primaryLanguage ? repo.primaryLanguage.color : "rgba(255, 255, 255, 0.5)" }}></span> {repo.primaryLanguage ? repo.primaryLanguage.name : "Markdown"}
             </span>
            </a>
           </NextLink>
           <NextLink key="repo_stars" href={`${repo.url}/stargazers`}>
            <a target="_blank">
-            <span className="snap-always snap-center flex w-max content-center items-center rounded-lg border-2 border-transparent bg-black/[5%] py-[0.12em] px-[0.5em] align-middle text-[88%] text-black/[60%] duration-200 motion-reduce:transition-none dark:bg-white/10 dark:text-white/[70%]">
+            <span className="flex w-max snap-center snap-always content-center items-center rounded-lg border-2 border-transparent bg-black/[5%] py-[0.12em] px-[0.5em] align-middle text-[88%] text-black/[60%] duration-200 motion-reduce:transition-none dark:bg-white/10 dark:text-white/[70%]">
              <svg xmlns="http://www.w3.org/2000/svg" className="mr-1 h-5 w-5 stroke-black/[50%] duration-200 motion-reduce:transition-none dark:stroke-white/[70%]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
              </svg>{" "}
@@ -171,7 +171,7 @@ export default function main({ repositories, user }) {
           </NextLink>
           <NextLink key="repo_forks" href={`${repo.url}/network/members`}>
            <a target="_blank">
-            <span className="snap-always snap-center flex w-max content-center items-center rounded-lg border-2 border-transparent bg-black/[5%] py-[0.12em] px-[0.5em] align-middle text-[88%] text-black/[60%] duration-200 motion-reduce:transition-none dark:bg-white/10 dark:text-white/[70%]">
+            <span className="flex w-max snap-center snap-always content-center items-center rounded-lg border-2 border-transparent bg-black/[5%] py-[0.12em] px-[0.5em] align-middle text-[88%] text-black/[60%] duration-200 motion-reduce:transition-none dark:bg-white/10 dark:text-white/[70%]">
              <svg xmlns="http://www.w3.org/2000/svg" className="mr-1 h-5 w-5 fill-black/[50%] duration-200 motion-reduce:transition-none dark:fill-white/[70%]" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
              </svg>{" "}
