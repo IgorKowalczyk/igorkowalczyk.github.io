c6f361456312d7547cff48d44c1e49d77a678d43
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-23 13:01:14 +0200

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -66,23 +66,19 @@ export default function Nav() {
    </div>
    <Transition
     as={Fragment}
-    enter="transition ease-out duration-100"
+    enter="transition ease-out duration-300"
     enterFrom="transform opacity-0 scale-95"
     enterTo="transform opacity-100 scale-100"
-    leave="transition ease-in duration-75"
+    leave="transition ease-in duration-100"
     leaveFrom="transform opacity-100 scale-100"
     leaveTo="transform opacity-0 scale-95"
    >
-   <Menu.Items className="border-[1px] border-white/[15%]  backdrop-blur-[9px] absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md dark:bg-opacity-70 bg-opacity-70 bg-white shadow-lg dark:bg-[#08152b] firefox:bg-opacity-100 dark:firefox:bg-opacity-100">
-            <div className="px-1 py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
+   <Menu.Items className="border-[1px] border-white/[15%] backdrop-blur-[9px] absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md dark:bg-opacity-70 bg-opacity-70 bg-white shadow-lg dark:bg-[#08152b] firefox:bg-opacity-100 dark:firefox:bg-opacity-100">
+    <div className="px-1 py-1 ">
+     <Menu.Item>
+      {({ active }) => (
+       <button className={`${active ? 'bg-violet-500' : ''} group flex w-full items-center rounded-md px-2 py-2 text-sm text-white duration-300`}>
+       {active ? (
                       <EditActiveIcon
                         className="mr-2 h-5 w-5"
                         aria-hidden="true"
