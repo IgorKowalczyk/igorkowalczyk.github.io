9b87bafc03c2e802d00d0535fe89468d2cebb5a8
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-23 11:57:58 +0200

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -62,10 +62,6 @@ export default function Nav() {
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             Options
-            <ChevronDownIcon
-              className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
-              aria-hidden="true"
-            />
           </Menu.Button>
         </div>
         <Transition
