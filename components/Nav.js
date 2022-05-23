d4a3106ff9da4d1dba5d08ca485151874d246668
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-23 12:01:16 +0200

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -57,7 +57,7 @@ export default function Nav() {
 
 
 
- <div className="fixed top-16 w-56 text-right">
+ <div className="w-56 text-right">
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
