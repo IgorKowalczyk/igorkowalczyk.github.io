dd378218a904decb2c790f24dfb226e5b487be6e
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-23 12:30:32 +0200

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -73,7 +73,7 @@ export default function Nav() {
     leaveFrom="transform opacity-100 scale-100"
     leaveTo="transform opacity-0 scale-95"
    >
-   <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+   <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white/[50%] dark:bg-dark/[50%] shadow-lg">
             <div className="px-1 py-1 ">
               <Menu.Item>
                 {({ active }) => (
