27988b8a96047219af7de147b332fe024d165924
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-23 13:28:50 +0200

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -79,6 +79,13 @@ export default function Nav() {
       {({ active }) => (
        <button className={`${active ? 'dark:bg-white/10 bg-black/10' : ''} group flex w-full items-center rounded-md px-2 py-2 text-sm text-black dark:text-white duration-300 motion-reduce:transition-none`}>
         Theme
+        <button aria-label="Toggle Dark Mode" type="button" className="mx-8 flex h-9 w-9 items-center justify-center rounded-lg border-2 border-transparent bg-transparent transition-all duration-300 hover:border-[#3391fc] hover:shadow-hoverLight motion-reduce:transition-none dark:bg-white/10 dark:hover:border-[#5686f5] dark:hover:shadow-hoverDark" onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}>
+         {mounted && (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="h-5 w-5 text-gray-800 dark:text-gray-200">
+           {resolvedTheme === "dark" ? <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /> : <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />}
+          </svg>
+         )}
+        </button>
        </button>
       )}
      </Menu.Item>
@@ -94,23 +101,6 @@ export default function Nav() {
    </Transition>
   </Menu>
  </div>
-
-
-
-
-
-
-
-
-    <Tippy content={resolvedTheme === "dark" ? "Enable light theme" : "Enable dark theme"} animation={"shift-away"} theme={"translucent"}>
-     <button aria-label="Toggle Dark Mode" type="button" className="mx-8 flex h-9 w-9 items-center justify-center rounded-lg border-2 border-transparent bg-transparent transition-all duration-300 hover:border-[#3391fc] hover:shadow-hoverLight motion-reduce:transition-none dark:bg-white/10 dark:hover:border-[#5686f5] dark:hover:shadow-hoverDark" onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}>
-      {mounted && (
-       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="h-5 w-5 text-gray-800 dark:text-gray-200">
-        {resolvedTheme === "dark" ? <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" /> : <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />}
-       </svg>
-      )}
-     </button>
-    </Tippy>
    </nav>
   </div>
  );
