cc7df0369a469b8fdb0ad52db862a0665846d430
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-25 13:06:29 +0200

    Update Arc.js

--- components/Arc.js
+++ components/Arc.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-export default class Arc extends Component {
+export class Arc extends Component {
  render() {
   if (process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_ARC_TOKEN) {
    return <script async src={"https://arc.io/widget.min.js#" + process.env.NEXT_PUBLIC_ARC_TOKEN}></script>;
