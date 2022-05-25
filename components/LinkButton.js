ca2a706905f229030a3b3a52dec16876d492f741
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-25 13:06:53 +0200

    Update LinkButton.js

--- components/LinkButton.js
+++ components/LinkButton.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import PropTypes from "prop-types";
 
-export default function LinkButton({ href, text, theme }) {
+export function LinkButton({ href, text, theme }) {
  const themes = {
   default: "border-black/[20%] dark:border-white/[15%] dark:bg-white/[5%] dark:hover:bg-white/[10%] text-black/[70%] dark:text-white",
   info: "border-sky-500 bg-sky-500/[10%] dark:border-sky-300 dark:bg-sky-100/[10%] dark:hover:bg-sky-100/[20%] text-black/[70%] dark:text-white",
