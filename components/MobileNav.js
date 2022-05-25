e4f0bc25d709f8863d9d86946cc87eda12f23cfd
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-05-25 13:17:36 +0200

    Update MobileNav.js

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -1,4 +1,3 @@
-import cn from "classnames";
 import Link from "next/link";
 import useDelayedRender from "use-delayed-render";
 import { useState } from "react";
@@ -31,7 +30,7 @@ export default function MobileMenu() {
     </svg>
    </button>
    {isMenuMounted && (
-    <div className={cn(isMenuRendered && "menuRendered", "absolute left-0 top-0 z-[1001] mt-[73px] flex h-screen w-3/4 flex-col opacity-0 backdrop-blur-[9px] duration-200 motion-reduce:transition-none md:hidden ")}>
+    <div className={`${isMenuRendered ? "menuRendered" : ""} absolute left-0 top-0 z-[1001] mt-[73px] flex h-screen w-3/4 flex-col opacity-0 backdrop-blur-[9px] duration-200 motion-reduce:transition-none md:hidden`}>
      <div className="h-full border-r-[1px] border-black/[10%] bg-white bg-opacity-70 px-3.5 shadow duration-200 firefox:bg-opacity-100 motion-reduce:transition-none dark:border-white/[15%] dark:bg-[#08152b] dark:bg-opacity-70 dark:shadow-2xl dark:firefox:bg-opacity-100">
       <div>
        {menuItems.array.map((item, index) => {
