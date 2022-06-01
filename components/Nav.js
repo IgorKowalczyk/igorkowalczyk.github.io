ede000e031cc4f402c5ea4df607d8c6b56887617
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-06-01 11:04:34 +0200

    Update nav

--- components/Nav.js
+++ components/Nav.js
@@ -36,7 +36,7 @@ export function Nav() {
       return <NavItem href={item.href} text={item.title} target={item.target} key={index} />;
      })}
      <Popover className="relative">
-      {({ open }) => (
+      {({ open, close }) => (
        <>
         <Popover.Button className={`${open ? "pseudo-border-active bg-gray-200 text-gray-800 dark:bg-white/10 dark:text-gray-200" : ""} pseudo-border group relative hidden rounded-lg p-1 font-poppins text-gray-600 outline-none transition-all duration-200 hover:bg-gray-200 hover:text-gray-800 motion-reduce:transition-none dark:text-gray-400 dark:hover:bg-white/10 dark:hover:text-gray-200 sm:px-3 sm:py-2 md:flex md:items-center`}>
          <span>More</span>
@@ -61,8 +61,8 @@ export function Nav() {
               </div>
              </a>
             </NextLink>
-            <NextLink href="/blog" key="github">
-             <a onClick={() => setIsPopoverShowing(false)} key="github_navbar" className="-m-3 mt-4 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-blue-50/80 dark:hover:bg-white/5">
+            <NextLink href="/blog" key="blog">
+             <a onClick={() => close()} key="blog_navbar" className="-m-3 mt-4 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-blue-50/80 dark:hover:bg-white/5">
               <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-blue-100 text-white dark:bg-white/10 dark:text-black sm:h-12 sm:w-12">
                <svg xmlns="http://www.w3.org/2000/svg" className="inline h-[24px] w-[24px] stroke-black duration-200 motion-reduce:transition-none dark:stroke-white/[70%]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
@@ -75,7 +75,7 @@ export function Nav() {
              </a>
             </NextLink>
             <NextLink href="/contact" key="contact_navbar">
-             <a onClick={() => setIsPopoverShowing(false)} key="solutions" className="-m-3 mt-4 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-blue-50/80 dark:hover:bg-white/5">
+             <a onClick={() => close()} key="contact" className="-m-3 mt-4 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-blue-50/80 dark:hover:bg-white/5">
               <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-blue-100 text-white dark:bg-white/10 dark:text-black sm:h-12 sm:w-12">
                <svg xmlns="http://www.w3.org/2000/svg" className="inline h-[24px] w-[24px] stroke-black duration-200 motion-reduce:transition-none dark:stroke-white/[70%]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -90,7 +90,7 @@ export function Nav() {
            </div>
            <div className="border-t-[1px] border-black/10 bg-white px-5 py-4 dark:border-white/10 dark:bg-[#08152b]">
             <NextLink href={`https://github.com/${config.social.github.username}`} key="github_external">
-             <a target="_blank" className="group flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none dark:hover:bg-white/5 ">
+             <a onClick={() => close()} target="_blank" className="group flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none dark:hover:bg-white/5 ">
               <span className="flex items-center">
                <div className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 opacity-50 duration-100 group-hover:opacity-90 motion-reduce:transition-none" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
