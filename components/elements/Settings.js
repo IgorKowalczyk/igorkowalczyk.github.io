f4ddabb515f31acaff9431b83e3e25f49e15dae6
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-06-12 23:44:00 +0200

    Format code

--- components/elements/Settings.js
+++ components/elements/Settings.js
@@ -43,8 +43,8 @@ export default function Settings(props) {
           <button type="button" className="group mt-2 ml-auto flex rounded-md border border-transparent bg-blue-100 px-4 py-2 font-poppins text-sm font-medium text-blue-900 duration-200 hover:bg-blue-200 motion-reduce:transition-none dark:bg-white/[10%] dark:text-white dark:hover:bg-white/[15%]" onClick={() => setIsOpen(false)}>
            Close{" "}
            <svg xmlns="http://www.w3.org/2000/svg" className="mt-[2px] ml-2 h-4 w-4 duration-200 group-hover:translate-x-1 motion-reduce:transition-none motion-reduce:group-hover:translate-x-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
-       </svg>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+           </svg>
           </button>
          </div>
         </Dialog.Panel>
--- pages/work.js
+++ pages/work.js
@@ -29,7 +29,8 @@ export default function gitub_repos({ repositories }) {
        level4: "#216e39",
       }}
      >
-      {/*<ReactTooltip html delayShow={100} delayHide={100} resizeHide={true} effect="float" backgroundColor="rgba(0, 0, 0)" />*/}     </GitHubCalendar>
+      {/*<ReactTooltip html delayShow={100} delayHide={100} resizeHide={true} effect="float" backgroundColor="rgba(0, 0, 0)" />*/}{" "}
+     </GitHubCalendar>
     </div>
    </div>
    <div className="xl-grid-cols-4 grid grid-cols-1 gap-y-10 gap-x-6 px-8 text-center font-poppins text-black dark:text-white md:grid-cols-2 md:gap-x-10 lg:grid-cols-3">
