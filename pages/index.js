9f3b9734c353a96fa601033f403340b259efb4cb
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-06-22 15:45:45 +0200

    Update icons

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { Container } from "@components/elements/Container";
 import { Transition } from "@headlessui/react";
 import { useState, useEffect } from "react";
 import { RepoCard } from "@components/elements/RepoCard";
-import { UsersIcon } from "@heroicons/react/outline";
+import { UsersIcon, StarIcon } from "@heroicons/react/outline";
 import { Contact } from "@components/elements/Contact";
 const Dots = dynamic(() => import("@components/decorations/Dots"));
 
@@ -101,9 +101,7 @@ export default function Main({ repositories, user }) {
      <p className="hidden font-semibold duration-200 motion-reduce:transition-none md:block">
       <NextLink href={`https://github.com/${config.social.github.username}`}>
        <a target="_blank" className="group inline-block text-center duration-200 hover:text-black motion-reduce:transition-none dark:hover:text-white">
-        <svg fill="currentColor" className="mr-1 inline h-[24px] w-[24px] fill-black/[50%] duration-200 group-hover:fill-black motion-reduce:transition-none dark:fill-white/[70%] dark:group-hover:fill-white">
-         <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"></path>
-        </svg>{" "}
+        <StarIcon className="mr-1 inline h-[22px] w-[22px] stroke-black/[50%] duration-200 group-hover:stroke-black motion-reduce:transition-none dark:stroke-white/[70%] dark:group-hover:stroke-white"/>{" "}
         {user.user_starred_repos} Starred repos
        </a>
       </NextLink>
