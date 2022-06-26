2eed609bf5547aaee33e5faa6d5f71b3cf9f3554
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-06-26 22:59:03 +0200

    Update comments

--- components/elements/Footer.js
+++ components/elements/Footer.js
@@ -17,6 +17,7 @@ export function Footer() {
       Igor Kowalczyk<span className="bg-gradient-to-r from-[#6310ff] to-[#1491ff] box-decoration-clone bg-clip-text text-fill-transparent dark:from-[#a2facf] dark:to-[#64acff]">.</span> <span className="mx-1 mr-2 rounded-lg bg-black/[7%] px-2 py-1 text-xs dark:bg-white/10">v{appVersion}</span>
      </p>
     </NextLink>
+    { /* --- */ }
     <div className="items-center space-x-6 lg:flex">
      <div className="mt-2 text-left text-gray-700 dark:text-gray-400 lg:mt-0 lg:text-right">
       <p className="font-semibold">&copy; 2019 - {new Date().getFullYear()} Igor Kowalczyk, All rights reserved.</p>
