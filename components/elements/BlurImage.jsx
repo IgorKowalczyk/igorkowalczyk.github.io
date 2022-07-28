0d3a6f2272c6c871b49056952fe3f78b9ca8990a
Igor Kowalczyk <majonezexe@protonmail.com>
2022-07-28 16:19:08 +0200

    Fix colors and format code

--- components/elements/BlurImage.jsx
+++ components/elements/BlurImage.jsx
@@ -3,5 +3,5 @@ import { useState } from "react";
 
 export function BlurImage({ image }) {
  const [isLoading, setLoading] = useState(true);
- return <Image src={image.url} alt={image.name} className={`${isLoading ? "scale-110 blur-2xl grayscale" : "scale-100 blur-0 grayscale-0"} bg-zinc-200 dark:bg-zinc-200/[15%] rounded-lg duration-200 group-hover:opacity-75`} onLoadingComplete={() => setLoading(false)} layout="fill" objectFit="cover" />;
+ return <Image src={image.url} alt={image.name} className={`${isLoading ? "scale-110 blur-2xl grayscale" : "scale-100 blur-0 grayscale-0"} rounded-lg bg-zinc-200 duration-200 group-hover:opacity-75 dark:bg-zinc-200/[15%]`} onLoadingComplete={() => setLoading(false)} layout="fill" objectFit="cover" />;
 }
