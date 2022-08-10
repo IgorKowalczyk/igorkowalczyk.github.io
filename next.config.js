c6a6505ad57f64d04403c366729aa4e3bc8c7b01
Igor Kowalczyk <majonezexe@protonmail.com>
2022-08-10 10:04:39 +0000

    [WIP] Switch to preact on production

--- next.config.js
+++ next.config.js
@@ -72,8 +72,13 @@ module.exports = withPlugins(
     },
    ];
   },
-  webpack: (config, { isServer, dev, config: { distDir } }) => {
-   if (!isServer && !dev) {
+  webpack: (config, { isServer, dev }) => {
+   if (!dev && !isServer) {
+    Object.assign(config.resolve.alias, {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+    })
     config.plugins.push(
      new CompressionPlugin(),
      new LodashModuleReplacementPlugin(),
