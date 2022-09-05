2a3997e5faf57d6afadae1274dd19cc7a541f482
Igor Kowalczyk <majonezexe@protonmail.com>
2022-09-05 11:06:47 +0000

    Format code

--- pages/uses.jsx
+++ pages/uses.jsx
@@ -10,10 +10,10 @@ export default function Uses({ uses }) {
   <Container title="Uses">
    <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
     <header>
-    <h1 className="my-6 flex flex-wrap items-center justify-center box-decoration-clone bg-clip-text text-center font-poppins text-[2rem] font-semibold motion-reduce:transition-none">
-     {uses.title}
-     <span className="bg-gradient-to-r from-[#6310ff] to-[#1491ff] box-decoration-clone bg-clip-text text-fill-transparent dark:from-[#a2facf] dark:to-[#64acff]">.</span>
-    </h1>
+     <h1 className="my-6 flex flex-wrap items-center justify-center box-decoration-clone bg-clip-text text-center font-poppins text-[2rem] font-semibold motion-reduce:transition-none">
+      {uses.title}
+      <span className="bg-gradient-to-r from-[#6310ff] to-[#1491ff] box-decoration-clone bg-clip-text text-fill-transparent dark:from-[#a2facf] dark:to-[#64acff]">.</span>
+     </h1>
     </header>
     <p className="pb-2 text-center font-poppins text-slate-600 dark:text-slate-400">{uses.description}</p>
     <section className="prose mt-8 w-full max-w-none dark:prose-dark">
