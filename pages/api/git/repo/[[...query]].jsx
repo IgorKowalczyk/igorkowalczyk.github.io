96d60c6d19981017039da44d8121ad97ba14439c
Igor Kowalczyk <majonezexe@protonmail.com>
2022-09-07 11:26:21 +0200

    Fix improper input validation

--- pages/api/git/repo/[[...query]].jsx
+++ pages/api/git/repo/[[...query]].jsx
@@ -7,8 +7,8 @@ import { social } from "@/config";
 
 const info = async (req, res) => {
  const { query } = req.query;
- const type = query[0].toLowerCase();
- const count = query[1].toLowerCase();
+ const type = (query[0] || "public").toLowerCase();
+ const count = (query[1] || "10").toLowerCase();
  if (type !== "private" && type !== "public") {
   return res.status(400).json({
    error: "Invalid type",
