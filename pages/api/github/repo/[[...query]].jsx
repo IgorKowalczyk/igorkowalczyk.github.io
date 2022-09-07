6fb321208eaf100c55e457908ab89ebf4cd31061
Igor Kowalczyk <majonezexe@protonmail.com>
2022-09-07 12:33:28 +0200

    Update [[...query]].jsx

--- pages/api/github/repo/[[...query]].jsx
+++ pages/api/github/repo/[[...query]].jsx
@@ -7,8 +7,8 @@ import { social } from "@/config";
 
 const info = async (req, res) => {
  const { query } = req.query;
- const type = query[0].toString().toLowerCase();
- const count = query[1].toString().toLowerCase();
+ const type = typeof query[0] === "string" ? query[0].toLowerCase() : "";
+ const count = typeof query[1] === "string" ? query[1].toLowerCase() : "";
  if (type !== "private" && type !== "public") {
   return res.status(400).json({
    error: "Invalid type",
