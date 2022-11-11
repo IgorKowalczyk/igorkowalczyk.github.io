c6612eacbc38bc3d58dc86669e810042184042f1
Igor Kowalczyk <majonezexe@protonmail.com>
2022-11-11 20:22:34 +0100

    Use SWR

--- pages/api/github/user/[[...query]].jsx
+++
@@ -1,5 +0,0 @@
-import { ApolloClient, createHttpLink, InMemoryCache, gql } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { social } from "@/config";
-
-const info = async (req, res) => {};
