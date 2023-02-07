80d733550a4a803ef79a319526499ae8a0031ce2
Igor Kowalczyk <majonezexe@protonmail.com>
2023-02-07 13:20:25 +0100

    Use hybrid rendering (SSR + SWR)

--- pages/api/github/repo/popular.jsx
+++ pages/api/github/repo/popular.jsx
@@ -1,63 +1,6 @@
-import { ApolloClient, createHttpLink, InMemoryCache, gql } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { social } from "/config";
+import { GetPopular } from "lib/graphQl";
 
-export default async function handler(req, res) {
- const httpLink = createHttpLink({
-  uri: "https://api.github.com/graphql",
- });
-
- const authLink = setContext((_, { headers }) => {
-  return {
-   headers: {
-    ...headers,
-    authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-   },
-  };
- });
-
- const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
- });
-
- const mostPopularRepos = await client.query({
-  query: gql`
-    {
-     user(login: \"${social.github.username}\") {
-       repositories(
-         isFork: false
-         isLocked: false
-         privacy: PUBLIC
-         first: 6
-         orderBy: {field: STARGAZERS, direction: DESC}
-         ownerAffiliations: OWNER
-       ) {
-         edges {
-           node {
-             ... on Repository {
-               name
-               url
-               owner {
-                 login
-               }
-               description
-               isArchived
-               forkCount
-               id
-               stargazerCount
-               primaryLanguage {
-                 name
-                 color
-               }
-             }
-           }
-         }
-       }
-     }
-   }
-`,
- });
-
- res.status(200).json(mostPopularRepos);
+export default async function handler(_, res) {
+ const data = await GetPopular();
+ res.status(200).json(data.repositories.edges);
 }
