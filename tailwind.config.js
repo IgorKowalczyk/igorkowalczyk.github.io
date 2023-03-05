8f2b99ea81edca3bbfa52d1562b4b2dc9a291b95
Igor Kowalczyk <majonezexe@protonmail.com>
2023-03-05 23:24:55 +0100

    Remove pages from tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
  darkMode: "class",
  content: [
   // Prettier
-  "./pages/**/*.{js,ts,jsx,tsx}",
   "./app/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",
   "./config.js",
