817a4f204c366d5b2dad5a6a09a7b1558bfa8906
Igor Kowalczyk <majonezexe@protonmail.com>
2023-03-07 23:18:26 +0100

    Fix hotjar script

--- app/layout.jsx
+++ app/layout.jsx
@@ -56,6 +56,24 @@ export default function RootLayout({ children }) {
  return (
   <ServerThemeProvider attribute="class">
    <html lang="en">
+    <head>
+    {process.env.HOTJAR_ID && (
+     <script
+      dangerouslySetInnerHTML={{
+       __html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+`,
+      }}
+     />
+    )}
+     </head>
     <body className={`bg-main-white dark:bg-main-dark ${inter.className}`}>
      <Nav />
      <main className="mt-24 flex min-h-screen flex-col px-6 antialiased">
