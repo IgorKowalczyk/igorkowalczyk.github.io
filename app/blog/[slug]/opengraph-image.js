d6d07a73bda58aabeec7d61999067c0835e10f4b
Igor Kowalczyk <majonezexe@protonmail.com>
2023-10-28 20:44:24 +0200

    Switch font from Inter to Geist

--- app/blog/[slug]/opengraph-image.js
+++ app/blog/[slug]/opengraph-image.js
@@ -19,8 +19,8 @@ export default async function Image({ params }) {
  const { slug } = params;
  const post = allBlogs.find((post) => post.slug === slug);
 
- const fontBold = await fetch(new URL("/public/fonts/bold.ttf", import.meta.url)).then((res) => res.arrayBuffer());
- const fontRegular = await fetch(new URL("/public/fonts/regular.ttf", import.meta.url)).then((res) => res.arrayBuffer());
+ const fontBold = await fetch(new URL("/public/fonts/Geist-Black.otf", import.meta.url)).then((res) => res.arrayBuffer());
+ const fontRegular = await fetch(new URL("/public/fonts/Geist-Regular.otf", import.meta.url)).then((res) => res.arrayBuffer());
 
  if (!post) {
   return redirect("/opengraph-image");
@@ -37,18 +37,18 @@ export default async function Image({ params }) {
      alignItems: "center",
      justifyContent: "center",
      backgroundColor: "#101110",
-     fontFamily: "PoppinsBold",
+     fontFamily: "Geist-Black",
      fontSize: 64,
      fontWeight: 900,
      boxShadow: "inset 0px 0px 277px 3px #101110",
      backgroundImage: "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(255,255,255,0.05)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e\")",
     }}
    >
-    <div style={{ color: "#fff", fontFamily: "PoppinsBold" }}>{post.title ?? ""}</div>
+    <div style={{ color: "#fff", fontFamily: "Geist-Black" }}>{post.title ?? ""}</div>
     <div
      style={{
       color: "rgba(255, 255, 255, 0.5)",
-      fontFamily: "PoppinsRegular",
+      fontFamily: "Geist-Regular",
       fontSize: 48,
       marginTop: "15px",
      }}
@@ -61,13 +61,13 @@ export default async function Image({ params }) {
    ...size,
    fonts: [
     {
-     name: "PoppinsBold",
+     name: "Geist-Black",
      data: fontBold,
      style: "normal",
      weight: 900,
     },
     {
-     name: "PoppinsRegular",
+     name: "Geist-Regular",
      data: fontRegular,
      style: "normal",
      weight: 400,
--- app/opengraph-image.js
+++ app/opengraph-image.js
@@ -13,8 +13,8 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Image() {
- const fontBold = await fetch(new URL("/public/fonts/bold.ttf", import.meta.url)).then((res) => res.arrayBuffer());
- const fontRegular = await fetch(new URL("/public/fonts/regular.ttf", import.meta.url)).then((res) => res.arrayBuffer());
+ const fontBold = await fetch(new URL("/public/fonts/Geist-Black.otf", import.meta.url)).then((res) => res.arrayBuffer());
+ const fontRegular = await fetch(new URL("/public/fonts/Geist-Regular.otf", import.meta.url)).then((res) => res.arrayBuffer());
 
  return new ImageResponse(
   (
@@ -27,18 +27,18 @@ export default async function Image() {
      alignItems: "center",
      justifyContent: "center",
      backgroundColor: "#101110",
-     fontFamily: "PoppinsBold",
+     fontFamily: "Geist-Black",
      fontSize: 64,
      fontWeight: 900,
      boxShadow: "inset 0px 0px 277px 3px #101110",
      backgroundImage: "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(255,255,255,0.05)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e\")",
     }}
    >
-    <div style={{ color: "#fff", fontFamily: "PoppinsBold" }}>{meta.title}</div>
+    <div style={{ color: "#fff", fontFamily: "Geist-Black" }}>{meta.title}</div>
     <div
      style={{
       color: "rgba(255, 255, 255, 0.5)",
-      fontFamily: "PoppinsRegular",
+      fontFamily: "Geist-Regular",
       fontSize: 48,
       marginTop: "15px",
      }}
@@ -51,13 +51,13 @@ export default async function Image() {
    ...size,
    fonts: [
     {
-     name: "PoppinsBold",
+     name: "Geist-Black",
      data: fontBold,
      style: "normal",
      weight: 900,
     },
     {
-     name: "PoppinsRegular",
+     name: "Geist-Regular",
      data: fontRegular,
      style: "normal",
      weight: 400,
