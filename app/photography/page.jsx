9e5b2fb68c617dc3eb7ea4db601e3d00496c8ecf
Igor Kowalczyk <majonezexe@protonmail.com>
2023-10-31 22:15:02 +0100

    Clarify text

--- app/photography/page.jsx
+++ app/photography/page.jsx
@@ -15,7 +15,7 @@ export default async function Photography() {
     My photography
     <span className="bg-gradient-to-r from-[#6310ff] to-[#1491ff] box-decoration-clone bg-clip-text text-fill-transparent dark:from-[#a2facf] dark:to-[#64acff]">.</span>
    </h1>
-   <p className="pb-2 text-slate-600 dark:text-slate-400">I call this page my second Instagram, here I upload photos taken by me in various places around the world. From Poland - my home country to Italy.</p>
+   <p className="pb-2 text-slate-600 dark:text-slate-400">I call this page my second Instagram, here I upload photos taken by me in various places around the world. From Poland (my home country) to even Italy.</p>
    <p className="pb-6 text-slate-600 dark:text-slate-400">I love traveling, I love taking pictures, from every trip I bring back hundreds of different photos. Here you can browse through the best ones</p>
 
    <div className="w-full columns-2 gap-6 pt-2 md:columns-2xs">
