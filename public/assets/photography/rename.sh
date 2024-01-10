88bb22b2a513511d26e15e25570652036f98afff
Igor Kowalczyk <majonezexe@protonmail.com>
2024-01-10 14:46:04 +0100

    Update photography page content

--- public/assets/photography/rename.sh
+++ public/assets/photography/rename.sh
@@ -1,16 +1,20 @@
 counter=1
 
-for file in $(ls -t); do
-    if [ "$file" = "$script_filename" ]; then
-        continue
-    fi
-
+for file in *; do
     if [ -f "$file" ]; then
         extension="${file##*.}"
+        if [ "$extension" = "sh" ]; then
+            continue
+        fi
+
+        while [ -e "${counter}.${extension}" ]; do
+            counter=$((counter + 1))
+        done
+
         new_name="${counter}.${extension}"
         mv "$file" "$new_name"
-        ((counter++))
+        counter=$((counter + 1))
     fi
 done
 
-echo "Images have been successfully renamed from newest to oldest!"
+echo "Images have been successfully renamed!"
