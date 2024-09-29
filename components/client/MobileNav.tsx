65d47adf1065fb48c885ea531d7e11bc40c95b9f
Igor Kowalczyk <majonezexe@protonmail.com>
2024-09-29 19:17:45 +0200

    Refactor MobileNav component and update global styles

--- components/client/MobileNav.tsx
+++ components/client/MobileNav.tsx
@@ -19,8 +19,8 @@ export default function MobileNav() {
    <Drawer.Portal>
     <Drawer.Overlay className="fixed inset-0 bg-black/50 dark:bg-[#161617]/70" />
     <Drawer.Content className="fixed inset-y-0 left-0 z-10 flex w-full max-w-96 !outline-none">
-     <Drawer.Title className="sr-only">Mobile navigation</Drawer.Title>
-     <div className="flex w-full max-w-96 flex-col border-r border-black/10 bg-white/90 p-6 backdrop-blur-xl duration-200 motion-reduce:transition-none dark:border-neutral-800 dark:bg-[#161617]/70">
+     <div className="flex w-[90%] max-w-96 flex-col border-r border-black/10 bg-white/90 p-6 backdrop-blur-xl duration-200 motion-reduce:transition-none dark:border-neutral-800 dark:bg-[#161617]/70">
+      <Drawer.Handle />
       <Drawer.Description className="mt-3 flex flex-col gap-2 divide-y divide-neutral-200 dark:divide-neutral-800">
        {menuItems.map((item) => {
         return (
