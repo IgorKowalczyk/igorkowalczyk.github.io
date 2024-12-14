d367d9fe5b86d6329e66a8e3f84bb9a9024beb9d
Igor Kowalczyk <majonezexe@protonmail.com>
2024-12-14 01:21:32 +0100

    Format code

--- app/actions/contact.ts
+++ app/actions/contact.ts
@@ -8,54 +8,52 @@ import { contactFormSchema } from "@/lib/validator";
 
 /* eslint-disable-next-line typescript/no-explicit-any */
 export async function submitContactForm(_prevState: any, data: FormData) {
-
  const formData = {
   email: data.get("email"),
   name: data.get("name"),
   message: data.get("message"),
  };
 
-   try {
-   const h = await headers();
-   const ip = h.get("x-forwarded-for") ?? "unknown";
+ try {
+  const h = await headers();
+  const ip = h.get("x-forwarded-for") ?? "unknown";
   const isRateLimited = rateLimit(ip);
 
- if (isRateLimited) return { error: "You are sending messages too frequently!" };
+  if (isRateLimited) return { error: "You are sending messages too frequently!" };
 
+  const result = contactFormSchema.safeParse(formData);
 
- const result = contactFormSchema.safeParse(formData);
+  if (!result.success) {
+   const errors = result.error.flatten().fieldErrors;
+   return { error: Object.values(errors).flat().join(" ") };
+  }
 
- if (!result.success) {
-  const errors = result.error.flatten().fieldErrors;
-  return { error: Object.values(errors).flat().join(" ") };
- }
+  const { name, email, message } = result.data;
+  const hash = createHash("sha256").update(email.trim()).digest("hex");
 
- const { name, email, message } = result.data;
- const hash = createHash("sha256").update(email.trim()).digest("hex");
-
- const embed = {
-  title: "📩 New message from igorkowalczyk.dev",
-  url: "https://igorkowalczyk.dev",
-  description: `>>> ${message.trim()}`,
-  color: 5759645,
-  fields: [
-   {
-    name: "🧑‍🦱 Name",
-    value: `\`\`\`${name.trim()}\`\`\``,
-    inline: true,
+  const embed = {
+   title: "📩 New message from igorkowalczyk.dev",
+   url: "https://igorkowalczyk.dev",
+   description: `>>> ${message.trim()}`,
+   color: 5759645,
+   fields: [
+    {
+     name: "🧑‍🦱 Name",
+     value: `\`\`\`${name.trim()}\`\`\``,
+     inline: true,
+    },
+    {
+     name: "📨 Email",
+     value: `\`\`\`${email.trim()}\`\`\``,
+     inline: true,
+    },
+   ],
+   footer: {
+    text: "igorkowalczyk.dev",
+    icon_url: `https://www.gravatar.com/avatar/${hash}?d=identicon`,
    },
-   {
-    name: "📨 Email",
-    value: `\`\`\`${email.trim()}\`\`\``,
-    inline: true,
-   },
-  ],
-  footer: {
-   text: "igorkowalczyk.dev",
-   icon_url: `https://www.gravatar.com/avatar/${hash}?d=identicon`,
-  },
-  timestamp: new Date().toISOString(),
- };
+   timestamp: new Date().toISOString(),
+  };
 
   const response = await fetch(`${process.env.DISCORD_WEBHOOK_URL}?wait=true`, {
    method: "POST",
@@ -70,8 +68,8 @@ export async function submitContactForm(_prevState: any, data: FormData) {
   const data = await response.json();
 
   if (!data.id) return { error: "Unable to send message" };
- return { message: "Your message has been sent successfully!" };
-  } catch (_error) {
+  return { message: "Your message has been sent successfully!" };
+ } catch (_error) {
   return { error: "Unable to send message" };
  }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -44,24 +44,24 @@ export function cn(...inputs: ClassValue[]) {
  return twMerge(clsx(inputs));
 }
 
-const idToRequestCount = new Map<string, number>(); 
+const idToRequestCount = new Map<string, number>();
 const rateLimiter = {
-  windowStart: Date.now(),
-  windowSize: 60 * 1000, // 1 minute
-  maxRequests: 1,
+ windowStart: Date.now(),
+ windowSize: 60 * 1000, // 1 minute
+ maxRequests: 1,
 };
 
 export const rateLimit = (ip: string) => {
-  const now = Date.now();
-  const isNewWindow = now - rateLimiter.windowStart > rateLimiter.windowSize;
-  if (isNewWindow) {
-    rateLimiter.windowStart = now;
-    idToRequestCount.set(ip, 0);
-  }
+ const now = Date.now();
+ const isNewWindow = now - rateLimiter.windowStart > rateLimiter.windowSize;
+ if (isNewWindow) {
+  rateLimiter.windowStart = now;
+  idToRequestCount.set(ip, 0);
+ }
 
-  const currentRequestCount = idToRequestCount.get(ip) ?? 0;
-  if (currentRequestCount >= rateLimiter.maxRequests) return true;
-  idToRequestCount.set(ip, currentRequestCount + 1);
+ const currentRequestCount = idToRequestCount.get(ip) ?? 0;
+ if (currentRequestCount >= rateLimiter.maxRequests) return true;
+ idToRequestCount.set(ip, currentRequestCount + 1);
 
-  return false;
+ return false;
 };
