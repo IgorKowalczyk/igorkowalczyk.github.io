bdbf69e3973d10d3c29ff1445ffbe4dfa4a94398
Igor Kowalczyk <majonezexe@protonmail.com>
2024-12-19 18:09:01 +0100

    Update alt text for technology icons to improve accessibility

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default async function HomePage() {
      {technologies.map((tech) => {
       return (
        <Link href={tech.link || "#"} key={`tech-link-${tech.name}`} className="flex cursor-pointer items-center gap-2 rounded-md border border-black/10 px-2 py-1 font-mono font-medium text-neutral-500 duration-200 hover:bg-black/5 motion-reduce:transition-none dark:border-neutral-800 dark:text-white/50 dark:hover:border-neutral-700 dark:hover:bg-white/5">
-        <Image src={tech.icon} alt={tech.name} width={20} height={20} className="size-5 rounded" /> {tech.name}
+        <Image src={tech.icon} alt={`${tech.name} logo`} width={20} height={20} className="size-5 rounded" /> {tech.name}
        </Link>
       );
      })}
